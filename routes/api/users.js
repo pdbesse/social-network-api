7aeb73bd6a3168dfe48c70aa13a38131a6d8ff8b
Phillip <phillip.besse@gmail.com>
2022-08-13 14:01:51 -0400

    more setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,1 +1,9 @@
 const router = require('express').Router();
+
+// /api/users
+// GET all, GET by _ID (included thoughts and friends), POST new, PUI by _id, DELETE by _id (remove thoughts as bonus)
+// /api/users/:userId/friends/:friendId
+// POST new friend to user's friend list, DELETE to remove friend from friend's list
+
+
+module.exports = router;
