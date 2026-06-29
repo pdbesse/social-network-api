b11297bf7f623916ad620d063839257dcaffbcf7
Phillip <phillip.besse@gmail.com>
2026-06-28 22:06:50 -0400

    update thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
+
 const {
-    getThought,
-    getOneThought,
-    createThought,
-    editThought,
-    deleteThought,
-    addReaction,
-    deleteReaction
-} = require('../../controllers/thought-controller')
+  getThoughts,
+  getOneThought,
+  createThought,
+  editThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../../controllers/thought-controller');
 
 // /api/thoughts
-router.route('/')
-.get(getThought)
-.post(createThought);
+router.route('/').get(getThoughts).post(createThought);
 
-router.route('/:thoughtId')
-.get(getOneThought)
-.put(editThought)
-.delete(deleteThought);
+router
+  .route('/:thoughtId')
+  .get(getOneThought)
+  .put(editThought)
+  .delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post(addReaction);
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
